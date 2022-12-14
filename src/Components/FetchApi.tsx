d9ff490210576a8747cd3e4c6faa801166771a0e
Mahdi Kamankesh <mehdikaman1074@gmail.com>
2022-12-14 15:27:16 +0100

    Finslipa

--- src/Components/FetchApi.tsx
+++ src/Components/FetchApi.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { questionAmount } from "../config";
 
 const FetchApi = () => {
   const [questions, setQuestions] = useState<any>([]);
   const [categories, setCategories] = useState<any>([]);
   const [category, setCategory] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<string>("");
+  const [correct, setCorrect] = useState<boolean>(false);
+  const [disable, setDisabled] = useState<boolean>();
+  const [numberQuestion, setNumberQuestion] = useState<number>(0);
+  const [showQuestion, setShowQuestion] = useState<boolean>(true);
 
   const slicedMathRandomNumber = categories.sort(() => Math.random() - 0.5);
 
@@ -12,16 +18,19 @@ const FetchApi = () => {
   }, []);
 
   useEffect(() => {
-    const getQuestion = async () => {
-      const response = await fetch(
-        `https://the-trivia-api.com/api/questions?categories=${category}&limit=1`
-      );
-      const data = await response.json();
-      setQuestions(data);
-      console.log(data);
-    };
-    getQuestion();
-  }, [category]);
+    if (categories && difficulty) {
+      const getQuestion = async () => {
+        const response = await fetch(
+          `https://the-trivia-api.com/api/questions?categories=${category}&limit=1&difficulty=${difficulty}`
+        );
+        const data = await response.json();
+        setQuestions(data);
+        setShowQuestion(false);
+        console.log(data);
+      };
+      getQuestion();
+    }
+  }, [category, difficulty, categories]);
 
   const getCategories = async () => {
     const categoriResponse = await fetch(
@@ -29,6 +38,28 @@ const FetchApi = () => {
     );
     const data = await categoriResponse.json();
     setCategories(Object.keys(data));
+  };
+
+  const changeDifficulty = (e: any) => {
+    let targetEl = e.target.value;
+    if (targetEl === "random") {
+      targetEl = "easy";
+    }
+    setDifficulty(targetEl);
+    console.log(targetEl);
+  };
+
+  const getQuestion = async () => {
+    const response = await fetch(
+      `https://the-trivia-api.com/api/questions?categories=${category}&limit=1&difficulty=${difficulty}`
+    );
+    const data = await response.json();
+    setQuestions(data);
+    setDisabled(false);
+    if (numberQuestion === questionAmount) {
+      console.log("done");
+    }
+    console.log(data);
   };
 
   const changeCategoryHandler = (e: any) => {
@@ -69,21 +100,58 @@ const FetchApi = () => {
     }
   };
 
+  const correctAnswerHandler = () => {
+    setDisabled(true);
+    setCorrect(true);
+    setNumberQuestion((prev) => prev + 1);
+  };
+  const falseAnswerHandler = () => {
+    setDisabled(true);
+    setCorrect(false);
+    setNumberQuestion((prev) => prev + 1);
+  };
   return (
     <>
-      <select
-        onChange={(e) => {
-          changeCategoryHandler(e);
-        }}
-        name={category}
-      >
-        {slicedMathRandomNumber.slice(0, 3).map((items: any) => {
-          return <option key={items.id}>{items}</option>;
-        })}
-      </select>
+      <div>
+        {!showQuestion && (
+          <h3>
+            Category: {category} & Difficulty: {difficulty}
+          </h3>
+        )}
+        {showQuestion && (
+          <select
+            onChange={(e) => {
+              changeCategoryHandler(e);
+            }}
+            name={category}
+          >
+            {slicedMathRandomNumber.slice(0, 3).map((items: any) => {
+              return <option key={items.id}>{items}</option>;
+            })}
+          </select>
+        )}
+        {!difficulty && (
+          <select
+            onChange={(e) => {
+              changeDifficulty(e);
+            }}
+            name={difficulty}
+          >
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+            <option value="random">random</option>
+          </select>
+        )}
+      </div>
+      <h5>
+        Questions answered : {numberQuestion}/{questionAmount}
+      </h5>
+
       {questions.slice(0, 3).map((item: any) => {
         return <p key={item.id}>{item.question}</p>;
       })}
+
       <div
         style={{
           display: "flex",
@@ -92,18 +160,43 @@ const FetchApi = () => {
         }}
       >
         {questions.map((item: any) => {
-          return <button key={item.id}>{item.correctAnswer}</button>;
+          return (
+            <button
+              onClick={correctAnswerHandler}
+              key={item.id}
+              style={{
+                backgroundColor: disable === true ? "green " : "",
+                color: disable === true ? "white " : "",
+              }}
+            >
+              {item.correctAnswer}
+            </button>
+          );
         })}
         {questions.map((item: any) => {
           return (
             <p key={item.id}>
               {item.incorrectAnswers.map((e: any) => {
-                return <button>{e}</button>;
+                return (
+                  <button
+                    onClick={falseAnswerHandler}
+                    style={{
+                      backgroundColor: disable === true ? "red " : "",
+                      color: disable === true ? "white " : "",
+                    }}
+                  >
+                    {e}
+                  </button>
+                );
               })}
             </p>
           );
         })}
       </div>
+      {disable && numberQuestion !== questionAmount && (
+        <button onClick={getQuestion}>next question</button>
+      )}
+      {numberQuestion === questionAmount && <button>show result</button>}
     </>
   );
 };
